fd07c1e3e6a690c68a3c2c0efe03704ed561a6ef
Josh <leong.jo@gmail.com>
2017-11-06 15:19:00 -0700

    fixed upper nav bar

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res, next) => {
+app.use('/', (req, res, next) => {
   res.render('index');
 
 })
+app.use('/javascript', (req, res, next) => {
+  res.render('javascript');
+
+})
+app.get('/javascript', (req, res, next) => {
+  res.render('javascript');
+
+})
+
 
 // app.use('/', routes);
 app.listen(3000, (req, res) => {
